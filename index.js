306d9373acbd3f9018d0d63e5b5d7e8aa31bbfb2
2255010087@kientruchanoi.edu.vn <2255010087@kientruchanoi.edu.vn>
2025-09-20 00:50:09 +0530

    push full project

--- index.js
+++ index.js
@@ -33,9 +33,11 @@ app.use(methodOverride("_method"));
 //end method
 app.locals.prefixAdmin=systemConfig.prefixAdmin;
 database.connect();
-app.set("views",`${__dirname}/views`)
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+// app.set("views",`${__dirname}/views`)
 app.set("view engine","pug")
-app.use(express.static(`${__dirname}/public`))
+// app.use(express.static(`${__dirname}/public`))
 
 //route
 router(app)
